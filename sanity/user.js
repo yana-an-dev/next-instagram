c9dd1aca54c5b11272e1a516c8da5a268f2dde42
Yana <ayh916@gmail.com>
2024-05-07 08:35:49 +1000

    set up sanity with schema

---
+++ sanity/user.js
@@ -0,0 +1,32 @@
+export default user = {
+	title: "User",
+	name: "user",
+	type: "document",
+	fields: [
+		{ title: "Username", name: "username", type: "string" },
+		{ title: "Name", name: "name", type: "string" },
+		{ title: "Email", name: "email", type: "string" },
+		{ title: "Image", name: "image", type: "string" },
+		{
+			title: "Following",
+			name: "following",
+			type: "array",
+			of: [{ type: "referrence", to: [{ type: "user" }] }],
+			validation: (Rule) => Rule.unique(),
+		},
+		{
+			title: "Follower",
+			name: "follower",
+			type: "array",
+			of: [{ type: "referrence", to: [{ type: "user" }] }],
+			validation: (Rule) => Rule.unique(),
+		},
+		{
+			title: "Bookmarks",
+			name: "bookmarks",
+			type: "array",
+			of: [{ type: "referrence", to: [{ type: "post" }] }],
+			validation: (Rule) => Rule.unique(),
+		},
+	],
+};
