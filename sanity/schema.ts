72e1b2ceb2281e260e76e93ed552919c7a2c0c89
Yana <ayh916@gmail.com>
2024-06-04 08:19:31 +1000

    sanity setup with mock data

--- sanity/schema.ts
+++
@@ -1,7 +0,0 @@
-import { type SchemaTypeDefinition } from "sanity";
-import user from "./user";
-import post from "./post";
-
-export const schema: { types: SchemaTypeDefinition[] } = {
-	types: [user, post],
-};
